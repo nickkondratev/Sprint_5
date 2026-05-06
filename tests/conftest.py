05a89f45bc9846db23f4f2399572d5be9ae6be1c
Никита <thisischestnutt@gmail.com>
2026-05-07 01:15:09 +0300

    Очистка main

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    yield driver
-    driver.quit()           
